a32432155d8093852f849755b7d44aac46e8e5a0
TheGamingKn1ght <145138445+TheGamingKn1ght@users.noreply.github.com>
2024-03-01 13:41:23 -0500

    AudioManager Added

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    #region Singleton
+    public static AudioManager Singleton;
+
+    public void Awake()
+    {
+        if (Singleton == null)
+        {
+            Singleton = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    #endregion
+
+    private List<AudioSource> audioSources = new List<AudioSource>();
+    [SerializeField] SoundEffect[] soundEffects;
+
+    private AudioSource CreateAudioSource()
+    {
+        GameObject audioSourceGO = new GameObject();
+        audioSourceGO.name = "audioSource";
+        audioSourceGO.transform.parent = this.transform;
+        AudioSource newAudioSource = audioSourceGO.AddComponent<AudioSource>();
+        audioSources.Add(newAudioSource);
+
+        return newAudioSource;
+    }
+    public void PlaySoundEffect(string soundName)
+    {
+
+        foreach (SoundEffect sfx in soundEffects)
+        {
+            if (sfx.soundName == soundName)
+            {
+                PlaySoundEffect(sfx);
+            }
+        }
+
+    }
+    public void PlaySoundEffect(SoundEffect sfx)
+    {
+        foreach (AudioSource audioSource in audioSources)
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.clip = sfx.audioClip;
+                audioSource.volume = sfx.volume;
+
+                audioSource.Play();
+                return;
+            }
+        }
+        AudioSource createdAudioSource = CreateAudioSource();
+        createdAudioSource.clip = sfx.audioClip;
+        createdAudioSource.volume = sfx.volume;
+        createdAudioSource.Play();
+
+    }
+}
+[System.Serializable]
+
+public struct SoundEffect
+{
+    public string soundName;
+    public AudioClip audioClip;
+    public float volume;
+
+}
