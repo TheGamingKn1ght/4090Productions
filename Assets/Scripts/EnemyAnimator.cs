38ba9b159b8046e1d283c838fb252d2165f7d776
TheGamingKn1ght <145138445+TheGamingKn1ght@users.noreply.github.com>
2024-03-27 22:26:32 -0400

    OpeningScene+MainMenu

---
+++ Assets/Scripts/EnemyAnimator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAnimator : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private NavMeshAgent agent;
+
+    private void LateUpdate()
+    {
+        UpdateLocalAnimator();
+    }
+
+    private void UpdateLocalAnimator()
+    {
+        animator.SetFloat("speedX", agent.speed);
+    }
+}
--- Assets/Scripts/SceneManager/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/SceneManager/MainMenu/MainMenuManager.cs
@@ -5,9 +5,30 @@ using Cinemachine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [Header("Cinemachine Cameras")]
     [SerializeField] private CinemachineVirtualCamera MainView;
     [SerializeField] private CinemachineVirtualCamera PlayView;
     [SerializeField] private CinemachineVirtualCamera SaveView;
+    
+    [Header("Canvas'")]
+    [SerializeField] private Canvas OpeningScreenCanvas;
 
+    public void ActivateSaveCamera()
+    {
+        SaveView.Priority = 2;
+        PlayView.Priority = 1;
+        MainView.Priority = 0;
+    }
+    public void ActivateMenuCamera()
+    {
+        OpeningScreenCanvas.gameObject.SetActive(false);
 
+        PlayView.Priority = 2;
+        MainView.Priority = 1;
+        SaveView.Priority = 0;
+    }
+    public void ReturnToLastCamera()
+    {
+
+    }
 }
